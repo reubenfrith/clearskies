7fc6252a55e05f41541b9851ebc207b1934f1ab0
Reuben Frith <reubenfrith007@gmail.com>
2026-03-01 23:23:52 +1100

    feat: handle 204 No Content response in apiFetch function

--- addin/src/lib/api.ts
+++ addin/src/lib/api.ts
@@ -33,6 +33,7 @@ async function apiFetch<T>(path: string, options?: RequestInit): Promise<T> {
     throw new Error(`API ${res.status}: ${await res.text()}`);
   }
 
+  if (res.status === 204) return undefined as T;
   return res.json() as Promise<T>;
 }
 
